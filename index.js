c614c71d43abf26c52fcf838f7652bc3ed0698eb
Siyamamkela <mpehlesiyamamkela@gmail.com>
2025-03-10 13:12:13 +0200

    Singleton done

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
-// 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+// 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance" should be null initially.
+const bankBranchInstance = null;
 // 2. Define a class called `BankBranch` for managing branch information.
+class BankBranch {
+  // 3. Create a constructor that takes `branchInfo` as a parameter.
+  constructor(branchInfo) {
+    // 4. Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
+    if (bankBranchInstance === null) {
+      // 5. If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
+      this.branchInfo = branchInfo;
+      bankBranchInstance = this;
+    }
+    // 6. Return the `bankBranchInstance` whether it's newly created or existing.
+    return bankBranchInstance;
+  }
+  // 7. Add methods to the `BankBranch` class for managing branch-related information.
+  getBranchInfo() {
+    return this.branchInfo;
+  }
+}
 
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
